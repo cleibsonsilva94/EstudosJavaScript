d7b9c64eedd8392ed397bb4faf9f65666804caef
cleibsonsilva94 <cleibsonslima@gmail.com>
2024-04-04 09:24:52 -0300

    Primeiro problema

---
+++ aula01/aula01PrimeiroDesafio.js
@@ -0,0 +1,10 @@
+/* 
+ Objetivo:
+ Fazer um programa que calcule o valor de uma viagem.
+ O programa terá 3 variáveis:
+ 1. Preço do combustível por litro.
+ 2. Gasto médio de combustível do carro por quilômetro.
+ 3. Distância percorrida pelo carro em quilômetros.
+*/
+const valorCombustivel = 5.79; // O Ponto substitue a virgula
+const consumoMedioDeLitro = 10;
