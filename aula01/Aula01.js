03e2944012d91bfc5c62f4436e89a1ccca228914
cleibsonsilva94 <cleibsonslima@gmail.com>
2024-04-03 22:46:00 -0300

    Aula 01 estudos java script

---
+++ aula01/Aula01.js
@@ -0,0 +1,26 @@
+
+// ANOTAÇÕES
+// Como declarar variáveis?
+// Existem 2 formas:
+// 'let' é um tipo de variável que pode ser alterado.
+// 'const' não pode ter seu valor substituído.
+
+
+let variave1 = 20 + 10;
+console.log (variave1); //comando usado para imprimir variaveis. 
+
+let variave2 = 50 + 10;
+console.log (variave2);
+
+// código
+// variavel1 = 50;
+// console.log(variavel1);
+
+// ANOTAÇÕES
+// 'const' não muda, e variável não muda.
+// "=" é uma ferramenta de atribuição.
+// Somadores matemáticos são:
+// "+" para somar,
+// "*" para multiplicar,
+// "-" para subtrair, e
+// "/" para dividir.
