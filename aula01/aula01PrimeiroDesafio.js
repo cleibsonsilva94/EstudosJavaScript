6e81eb04ed77cb23398babee447f03748a7b008b
cleibsonsilva94 <cleibsonslima@gmail.com>
2024-04-04 22:51:13 -0300

    Aula 01, primeiro problema

--- aula01/aula01PrimeiroDesafio.js
+++ aula01/aula01PrimeiroDesafio.js
@@ -6,5 +6,12 @@
  2. Gasto médio de combustível do carro por quilômetro.
  3. Distância percorrida pelo carro em quilômetros.
 */
+
+//entrada de dados
 const valorCombustivel = 5.79; // O Ponto substitue a virgula
-const consumoMedioDeLitro = 10;
+const kmPorLitro = 10;
+const distanciaEmKm = 100; 
+//Distancia para separar a entrada de dados, acima e abaixo o calculo. 
+const litrosConsumidos = distanciaEmKm / kmPorLitro;
+const valorGasto = litrosConsumidos * valorCombustivel;
+console.log(valorGasto);
