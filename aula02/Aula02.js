85a85cd180946a979277c370440f911fb3fe4bd0
cleibsonsilva94 <cleibsonslima@gmail.com>
2024-04-06 23:07:36 -0300

    Conceito de condicionais

--- aula02/Aula02.js
+++ aula02/Aula02.js
@@ -5,14 +5,18 @@
 // Ao usar ===, verifica se o resto é igual a 0 (true) ou diferente de 0 (false).
 // O operador === é o operador de igualdade estrita em JavaScript. Ele verifica se dois valores são iguais em tipo e valor. Dois iguais também funcionam. A diferença é que ele pode converter um texto para um valor numérico.
 // ! símbolo de negação.
-const numero = 12;
+const numero = 2;
 const eDivisivelPor = (numero % 5) === 0;
 
-if (eDivisivelPor) {
+if (numero === 0) {
+    console.log('O numero não e valido');
+}
+
+else if (eDivisivelPor) { // quantos poder.
     console.log('Sim');
 }
 
-else {
+else { //else no meio do nada não funciona. Ele é sempre no fim. 
     console.log('Não');
 }
 
