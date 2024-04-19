b590f5061ac0764e6f2e73575c0754c5ebcfd312
cleibsonsilva94 <cleibsonslima@gmail.com>
2024-04-18 23:19:17 -0300

    Aula 03 Exercicio 01

---
+++ aula03/Aula03Exercicios.js
@@ -0,0 +1,21 @@
+/* Exercicio da media ponderada de uma aluno
+
+*/
+
+//Entradas
+const nota1 = 10;
+const nota2 = 8;
+const nota3 = 1;
+const Media = (nota1 + nota2 + nota3) / 3;
+
+//Retorno
+if (Media < 5) {
+    console.log(`O aluno está reprovado pois sua media foi ${Media.toFixed(1)}`); //Se deixado vazio, ele arredonda parta cima. 
+}
+else if (Media > 5 && Media < 8) {
+    console.log(`O aluno está em recuperação pois sua media foi ${Media.toFixed(1)}`);
+}
+else {
+    console.log(`O aluno está aprovado e sua média foi ${Media.toFixed(1)}`);
+}
+
