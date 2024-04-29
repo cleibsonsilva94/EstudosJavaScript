a6b13a3fcd668c637ce4749836a5cb5352af687d
cleibsonsilva94 <cleibsonslima@gmail.com>
2024-04-29 17:39:28 -0300

    Final da atividade

--- aula03/Aula03Exercicio02.js
+++ aula03/Aula03Exercicio02.js
@@ -1,17 +1,24 @@
-// CALCULO DO IMC
+// CÁLCULO DO IMC
 
-//Recibimento de dados
+// Recebimento de dados
 const altura = 1.76;
-const peso = 85;
-//calculo
+const peso = 120;
+// Cálculo
 const IMC = peso / (altura * altura);
-//Manipulação baseada em condicionais
+// Manipulação baseada em condicionais
 if (IMC < 18.5) {
-  console.log(`Voce esta abaixo do peso, seu IMC é ${IMC.toFixed(1)}`);
+  console.log(`Você está abaixo do peso, seu IMC é ${IMC.toFixed(1)}`);
 }
 else if (IMC >= 18.5 && IMC < 25) {
-    console.log(`Voce esta com o peso ideal, seu IMC é ${IMC.toFixed(1)}`);
+    console.log(`Você está com o peso ideal, seu IMC é ${IMC.toFixed(1)}`);
 }
 else if (IMC >= 25 && IMC < 30) {
-    console.log(`Voce esta acima do peso, seu IMC é ${IMC.toFixed(1)}`);
+    console.log(`Você está acima do peso, seu IMC é ${IMC.toFixed(1)}`);
 }
+else if (IMC >= 30 && IMC < 40) {
+  console.log(`Você está obeso, seu IMC é ${IMC.toFixed(1)}`);
+}
+else {
+  console.log(`Seu nível de obesidade está grave, seu IMC é ${IMC.toFixed(1)}`)  
+}
+// Fim do código.
